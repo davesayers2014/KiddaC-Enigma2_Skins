7e440818e626a71a3c02cd834110c0def827d683
kiddac <kiddac2015@gmail.com>
2020-07-15 20:38:35 +0100

    Made openvix compatible

--- 1080_Skins/kiddac-shared-skin-components/kiddac-shared-skin-components/usr/lib/enigma2/python/Components/Converter/SlykServiceInfo.py
+++ 1080_Skins/kiddac-shared-skin-components/kiddac-shared-skin-components/usr/lib/enigma2/python/Components/Converter/SlykServiceInfo.py
@@ -11,7 +11,7 @@ from os import path
 WIDESCREEN = [1, 3, 4, 7, 8, 0xB, 0xC, 0xF, 0x10]
 
 
-class SlykServiceInfo(Poll, Converter, object):
+class SlykServiceInfo(Converter, object):
     HAS_TELETEXT = 1
     IS_MULTICHANNEL = 2
     AUDIO_STEREO = 3
@@ -43,21 +43,12 @@ class SlykServiceInfo(Poll, Converter, object):
     IS_576 = 29
     IS_480 = 30
     IS_4K = 31
-    IS_IPSTREAM = 32
-    IS_SDR = 33
-    IS_HDR = 34
-    IS_HDR10 = 35
-    IS_HLG = 36
-    IS_HDHDR = 37
-    FREQ_INFO = 38
-    PROGRESSIVE = 39
-    VIDEO_INFO = 40
+    FREQ_INFO = 32
+    PROGRESSIVE = 33
+    VIDEO_INFO = 34
 
     def __init__(self, type):
-        Poll.__init__(self)
         Converter.__init__(self, type)
-        self.poll_interval = 10000
-        self.poll_enabled = True
         self.type, self.interesting_events = {
             "HasTelext": (self.HAS_TELETEXT, (iPlayableService.evUpdatedInfo,)),
             "IsMultichannel": (self.IS_MULTICHANNEL, (iPlayableService.evUpdatedInfo,)),
@@ -76,7 +67,7 @@ class SlykServiceInfo(Poll, Converter, object):
             "TsId": (self.TSID, (iPlayableService.evUpdatedInfo,)),
             "OnId": (self.ONID, (iPlayableService.evUpdatedInfo,)),
             "Sid": (self.SID, (iPlayableService.evUpdatedInfo,)),
-            "Framerate": (self.FRAMERATE, (iPlayableService.evVideoSizeChanged, iPlayableService.evUpdatedInfo,)),
+			"Framerate": (self.FRAMERATE, (iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
             "Progressive": (self.PROGRESSIVE, (iPlayableService.evVideoProgressiveChanged, iPlayableService.evUpdatedInfo,)),
             "VideoInfo": (self.VIDEO_INFO, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evUpdatedInfo,)),
             "TransferBPS": (self.TRANSFERBPS, (iPlayableService.evUpdatedInfo,)),
@@ -87,35 +78,15 @@ class SlykServiceInfo(Poll, Converter, object):
             "Editmode": (self.EDITMODE, (iPlayableService.evUpdatedInfo,)),
             "IsStream": (self.IS_STREAM, (iPlayableService.evUpdatedInfo,)),
             "IsSD": (self.IS_SD, (iPlayableService.evVideoSizeChanged,)),
-            "IsHD": (self.IS_HD, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
+            "IsHD": (self.IS_HD, (iPlayableService.evVideoSizeChanged,)),
             "Is1080": (self.IS_1080, (iPlayableService.evVideoSizeChanged,)),
             "Is720": (self.IS_720, (iPlayableService.evVideoSizeChanged,)),
             "Is576": (self.IS_576, (iPlayableService.evVideoSizeChanged,)),
             "Is480": (self.IS_480, (iPlayableService.evVideoSizeChanged,)),
-            "Is4K": (self.IS_4K, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
-            "IsIPStream": (self.IS_IPSTREAM, (iPlayableService.evUpdatedInfo,)),
-            "IsSDR": (self.IS_SDR, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
-            "IsHDR": (self.IS_HDR, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
-            "IsHDR10": (self.IS_HDR10, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
-            "IsHLG": (self.IS_HLG, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
-            "IsHDHDR": (self.IS_HDHDR, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoGammaChanged,)),
-         }[type]
-        self.interesting_events += (iPlayableService.evStart,)
-
-    def _isHDMIIn(self, info):
-        return eServiceReference(info.getInfoString(iServiceInformation.sServiceref)).type == eServiceReference.idServiceHDMIIn
+            "Is4K": (self.IS_4K, (iPlayableService.evVideoSizeChanged,)),
+        }[type]
 
     def getServiceInfoString(self, info, what, convert=lambda x: "%d" % x):
-        if self._isHDMIIn(info):
-            return "N/A"
-        v = info.getInfo(what)
-        if v == -1:
-            return "N/A"
-        if v == -2:
-            return info.getInfoString(what)
-        return convert(v)
-
-    def getServiceInfoHexString(self, info, what, convert=lambda x: "%04x" % x):
         v = info.getInfo(what)
         if v == -1:
             return "N/A"
@@ -137,10 +108,7 @@ class SlykServiceInfo(Poll, Converter, object):
         return val
 
     def _getVal(self, pathname, info, infoVal, base=10):
-        if self._isHDMIIn(info):
-            return None
         val = self._getProcVal(pathname, base=base)
-
         return val if val is not None else info.getInfo(infoVal)
 
     def _getValInt(self, pathname, info, infoVal, base=10, default=-1):
@@ -148,10 +116,7 @@ class SlykServiceInfo(Poll, Converter, object):
         return val if val is not None else default
 
     def _getValStr(self, pathname, info, infoVal, base=10, convert=lambda x: "%d" % x):
-        if self._isHDMIIn(info):
-            return "N/A"
         val = self._getProcVal(pathname, base=base)
-
         return convert(val) if val is not None else self.getServiceInfoString(info, infoVal, convert)
 
     def _getVideoHeight(self, info):
@@ -184,31 +149,15 @@ class SlykServiceInfo(Poll, Converter, object):
         info = service and service.info()
         if not info:
             return False
-
         video_height = None
-        video_width = None
         video_aspect = None
         video_height = self._getVideoHeight(info)
-        video_width = self._getVideoWidth(info)
-
-        if path.exists("/proc/stb/vmpeg/0/aspect"):
-            f = open("/proc/stb/vmpeg/0/aspect", "r")
-            try:
-                video_aspect = int(f.read())
-            except:
-                pass
-            f.close()
-
-        if not video_height:
-            video_height = int(info.getInfo(iServiceInformation.sVideoHeight))
-        if not video_aspect:
-            video_aspect = info.getInfo(iServiceInformation.sAspect)
+        video_aspect = info.getInfo(iServiceInformation.sAspect)
 
         if self.type == self.HAS_TELETEXT:
             tpid = info.getInfo(iServiceInformation.sTXTPID)
             return tpid != -1
         elif self.type in (self.IS_MULTICHANNEL, self.AUDIO_STEREO):
-            # FIXME. but currently iAudioTrackInfo doesn't provide more information.
             audio = service.audioTracks()
             if audio:
                 n = audio.getNumberOfTracks()
@@ -216,7 +165,6 @@ class SlykServiceInfo(Poll, Converter, object):
                 while idx < n:
                     i = audio.getTrackInfo(idx)
                     description = i.getDescription()
-
                     if description and description.split()[0] in ("AC3", "AC-3", "AC3+", "DTS"):  # some audio description has 'audio' as additional value (e.g. 'AC-3 audio')
                         if self.type == self.IS_MULTICHANNEL:
                             return True
@@ -240,7 +188,7 @@ class SlykServiceInfo(Poll, Converter, object):
             return info.getInfoString(iServiceInformation.sHBBTVUrl) != ""
         elif self.type == self.AUDIOTRACKS_AVAILABLE:
             audio = service.audioTracks()
-            return audio and audio.getNumberOfTracks() > 1
+            return bool(audio) and audio.getNumberOfTracks() > 1
         elif self.type == self.SUBTITLES_AVAILABLE:
             subtitle = service and service.subtitle()
             subtitlelist = subtitle and subtitle.getSubtitleList()
@@ -254,10 +202,7 @@ class SlykServiceInfo(Poll, Converter, object):
         elif self.type == self.IS_SD:
             return video_height < 720
         elif self.type == self.IS_HD:
-            if info.getInfo(iServiceInformation.sGamma) > 0:
-                return False
-            else:
-                return video_height >= 720 and video_height < 2160
+            return video_height >= 720 and video_height < 2160
         elif self.type == self.IS_1080:
             return video_height >= 1080 and video_height <= 2119
         elif self.type == self.IS_720:
@@ -267,25 +212,10 @@ class SlykServiceInfo(Poll, Converter, object):
         elif self.type == self.IS_480:
             return video_height > 0 and video_height <= 575
         elif self.type == self.IS_4K:
-            if info.getInfo(iServiceInformation.sGamma) > 0:
-                return False
-            else:
-                return video_height >= 2160 and video_height < 4320
-        elif self.type == self.IS_IPSTREAM:
-            return service.streamed() is not None
-        elif self.type == self.IS_SDR:
-            return video_width > 2160 and video_width <= 3840 and info.getInfo(iServiceInformation.sGamma) == 0
-        elif self.type == self.IS_HDR:
-            return video_width > 2160 and video_width <= 3840 and info.getInfo(iServiceInformation.sGamma) == 1
-        elif self.type == self.IS_HDR10:
-            return video_width > 2160 and video_width <= 3840 and info.getInfo(iServiceInformation.sGamma) == 2
-        elif self.type == self.IS_HLG:
-            return video_width > 2160 and video_width <= 3840 and info.getInfo(iServiceInformation.sGamma) == 3
-        elif self.type == self.IS_HDHDR:
-            return video_width >= 721 and video_width < 1980 and info.getInfo(iServiceInformation.sGamma) > 0
+            return video_height >= 2160 and video_height < 4320
         elif self.PROGRESSIVE:
             return bool(self._getProgressive(info))
-
+        else:
             return False
 
     boolean = property(getBoolean)
@@ -296,7 +226,6 @@ class SlykServiceInfo(Poll, Converter, object):
         info = service and service.info()
         if not info:
             return ""
-
         if self.type == self.XRES:
             return self._getVideoWidthStr(info)
         elif self.type == self.YRES:
@@ -316,26 +245,13 @@ class SlykServiceInfo(Poll, Converter, object):
         elif self.type == self.ONID:
             return self.getServiceInfoString(info, iServiceInformation.sONID)
         elif self.type == self.SID:
-            return self.getServiceInfoHexString(info, iServiceInformation.sSID)
+            return self.getServiceInfoString(info, iServiceInformation.sSID)
         elif self.type == self.FRAMERATE:
-            video_rate = None
-            if path.exists("/proc/stb/vmpeg/0/framerate"):
-                f = open("/proc/stb/vmpeg/0/framerate", "r")
-                try:
-                    video_rate = int(f.read())
-                except:
-                    pass
-                f.close()
-            if not video_rate:
-                try:
-                    video_rate = int(self.getServiceInfoString(info, iServiceInformation.sFrameRate))
-                except:
-                    return "N/A fps"
-            return video_rate, lambda x: "%d fps" % ((x+500)/1000)
+            return self._getFrameRateStr(info, convert=lambda x: "%d fps" % ((x + 500) / 1000))
         elif self.type == self.PROGRESSIVE:
             return self._getProgressiveStr(info)
         elif self.type == self.TRANSFERBPS:
-            return self.getServiceInfoString(info, iServiceInformation.sTransferBPS, lambda x: "%d kB/s" % (x/1024))
+            return self.getServiceInfoString(info, iServiceInformation.sTransferBPS, lambda x: "%d kB/s" % (x / 1024))
         elif self.type == self.HAS_HBBTV:
             return info.getInfoString(iServiceInformation.sHBBTVUrl)
         elif self.type == self.FREQ_INFO:
@@ -363,8 +279,7 @@ class SlykServiceInfo(Poll, Converter, object):
             out = "Freq: %s %s %s %s %s" % (frequency, polarization, sr_txt, symbolrate, fec)
             return out
         elif self.type == self.VIDEO_INFO:
-            if self._isHDMIIn(info):
-                    return ""
+
             progressive = self._getProgressiveStr(info)
             fieldrate = self._getFrameRate(info)
             if fieldrate > 0:
@@ -386,41 +301,12 @@ class SlykServiceInfo(Poll, Converter, object):
             return -1
 
         if self.type == self.XRES:
-            video_width = None
-            if path.exists("/proc/stb/vmpeg/0/xres"):
-                f = open("/proc/stb/vmpeg/0/xres", "r")
-                try:
-                    video_width = int(f.read(), 16)
-                except:
-                    video_width = None
-                f.close()
-            if not video_width:
-                video_width = info.getInfo(iServiceInformation.sVideoWidth)
-            return str(video_width)
+            return str(self._getVideoWidth(info))
         elif self.type == self.YRES:
-            video_height = None
-            if path.exists("/proc/stb/vmpeg/0/yres"):
-                f = open("/proc/stb/vmpeg/0/yres", "r")
-                try:
-                    video_height = int(f.read(), 16)
-                except:
-                    video_height = None
-                f.close()
-            if not video_height:
-                video_height = info.getInfo(iServiceInformation.sVideoHeight)
-            return str(video_height)
+            return str(self._getVideoHeight(info))
         elif self.type == self.FRAMERATE:
-            video_rate = None
-            if path.exists("/proc/stb/vmpeg/0/framerate"):
-                f = open("/proc/stb/vmpeg/0/framerate", "r")
-                try:
-                    video_rate = f.read()
-                except:
-                    pass
-                f.close()
-            if not video_rate:
-                video_rate = info.getInfo(iServiceInformation.sFrameRate)
-            return str(video_rate)
+            return str(self._getFrameRate(self, info))
+
 
         return -1
 
@@ -428,12 +314,4 @@ class SlykServiceInfo(Poll, Converter, object):
 
     def changed(self, what):
         if what[0] != self.CHANGED_SPECIFIC or what[1] in self.interesting_events:
-            # Only want to update on iPlayableService.evStart
-            # if the service is HDMI IN.
-            if len(what) > 1 and what[1] == iPlayableService.evStart:
-                service = self.source.service
-                info = service and service.info()
-                if info and not self._isHDMIIn(info):
-                    return
-
             Converter.changed(self, what)
